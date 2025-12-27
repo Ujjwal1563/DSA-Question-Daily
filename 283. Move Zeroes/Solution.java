daa6d36b5c76bf0f6df9fd52d2114eeeebb71883
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2025-12-27 19:31:12 +0530

    283. Move Zeroes

---
+++ 283. Move Zeroes/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        for (int right =0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp = nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+            }
+        }
+    }
+}
+
+//[0,1,0,3,12]
+//[1,3,12,0,0]
