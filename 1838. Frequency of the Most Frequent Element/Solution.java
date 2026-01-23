3407f3c75c34b0ea426cff3851728b051b7f9d91
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-01-23 19:08:31 +0530

    Day 26

---
+++ 1838. Frequency of the Most Frequent Element/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+        public int maxFrequency(int[] nums, long k) {
+        int i = 0, j;
+        Arrays.sort(nums);
+        for (j = 0; j < nums.length; ++j) {
+            k += nums[j];
+            if (k < (long)nums[j] * (j - i + 1))
+                k -= nums[i++];
+        }
+        return j - i;
+    }
+}
