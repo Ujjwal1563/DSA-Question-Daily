b8dc1168a33c0f186931e8dfa781b9bd95e1d910
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-01-25 21:32:33 +0530

    Day 28

---
+++ 21. Merge Two Sorted Lists/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode  mergeList1 = new ListNode();
+        ListNode  mergeList = mergeList1;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                mergeList.next = list1;
+                list1 = list1.next;
+                mergeList = mergeList.next;
+            }
+            else {
+                mergeList.next = list2;
+                list2 = list2.next;
+                mergeList = mergeList.next;
+            }
+        }
+        while(list1 != null){
+            mergeList.next = list1;
+            list1 = list1.next;
+            mergeList = mergeList.next;
+        }
+        while(list2 != null){
+            mergeList.next = list2;
+            list2 = list2.next;
+            mergeList = mergeList.next;
+        }
+      return mergeList1.next; 
+    }
+}
