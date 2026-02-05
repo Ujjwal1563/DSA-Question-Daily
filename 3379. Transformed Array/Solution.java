74556fc3c5c6f806ebc0fcdf713906d9f4ced176
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-02-05 19:20:40 +0530

    Day 38 & Day 39

---
+++ 3379. Transformed Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result[i] = nums[(nums[i] + i) % n];
+            } else {
+                result[i] = nums[((i - Math.abs(nums[i])) % n + n) % n];
+            }
+        }
+        return result;
+    }
+}
