63b7132e5483d0b7b47c855cff42a517bfdabb35
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-02-08 19:42:19 +0530

    Day 42

---
+++ 110. Balanced Binary Tree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if(left == -1 || right == -1 || Math.abs(left-right)>1) return -1;
+        return Math.max(right, left) + 1;
+    } 
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        if(height(root) == -1) return false;
+        return true; 
+    }
+}
