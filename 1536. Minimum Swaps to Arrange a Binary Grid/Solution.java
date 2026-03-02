2e5ff4760e1402fc16f1851d9dc760220717e216
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-03-02 19:47:30 +0530

    Day 64

---
+++ 1536. Minimum Swaps to Arrange a Binary Grid/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSwaps(int[][] grid) {
+        int n = grid.length;
+        int[] endZeros = new int[n];
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--) {
+                count++;
+            }
+            endZeros[i] = count;
+        }
+        int steps = 0;
+        for (int i = 0; i < n; i++) {
+            int need = n - i - 1;
+            int j = i;
+            while (j < n && endZeros[j] < need) {
+                j++;
+            }
+            if (j == n) {
+                return -1;
+            }
+            steps += j - i;
+            while (j > i) {
+                int temp = endZeros[j];
+                endZeros[j] = endZeros[j - 1];
+                endZeros[j - 1] = temp;
+                j--;
+            }
+        }
+
+        return steps;
+    }
+}
